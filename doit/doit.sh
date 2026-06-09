539fd16697f6af93b1ebfab0c7a0bedc05e758d3
Marcin Wiącek <marcin@mwiacek.com>
2026-06-09 21:57:11 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,6 +1,24 @@
+# Part of PLLINUX
+# It's creating some binaries from the source
+# Tested on Lubuntu 26.04. It's possible, that some deps are missed
+
 package="bwrap";
 deps=0;
 cpu_num=6;
+
+download_unpack() {
+  url=$1
+  localfile=${url##*/}
+  package=$2
+  unpackeddir=$3
+  if [ ! -f "download/$localfile" ]; then wget -O download/$localfile $url fi
+  if [ ! -d "out/$package/$unpackeddir" ]; then
+    mkdir out/$package || true
+    cd out/$package
+    tar -xvf ../../download/$localfile
+    cd ../..
+  fi
+}
 
 mkdir app || true
 mkdir out || true
@@ -8,15 +26,7 @@ mkdir download || true
 if [ "$package" == "all" ] || [ "$package" == "kernel" ]; then
   if [ "$deps" == "1" ]; then sudo apt install build-essential libncurses-dev bc libelf-dev bison; fi
   ver="7.0.12";
-  if [ ! -f "download/linux-$ver.tar.xz" ]; then
-    wget -O download/linux-$ver.tar.xz https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-$ver.tar.xz
-  fi
-  if [ ! -d "out/kernel/linux-$ver" ]; then
-    mkdir out/kernel || true
-    cd out/kernel
-    tar -xvf ../../download/linux-$ver.tar.xz
-    cd ../..
-  fi
+  download_unpack https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-$ver.tar.xz kernel linux-$ver
   cp in/kernel/.config out/kernel/linux-$ver
   cd out/kernel/linux-$ver
   make -j$cpu_num
@@ -30,15 +40,7 @@ if [ "$package" == "all" ] || [ "$package" == "kernel" ]; then
 fi
 if [ "$package" == "all" ] || [ "$package" == "busybox" ]; then
   ver="1.38.0";
-  if [ ! -f "download/busybox-$ver.tar.bz2" ]; then
-    wget -O download/busybox-$ver.tar.bz2 https://busybox.net/downloads/busybox-$ver.tar.bz2
-  fi
-  if [ ! -d "out/busybox/busybox-$ver" ]; then
-    mkdir out/busybox || true
-    cd out/busybox
-    tar -xvf ../../download/busybox-$ver.tar.bz2
-    cd ../..
-  fi
+  download_unpack https://busybox.net/downloads/busybox-$ver.tar.bz2 busybox busybox-$ver
   mkdir app/busybox || true
   mkdir app/busybox/$ver || true
   cp in/busybox/.config out/busybox/busybox-$ver
@@ -52,15 +54,7 @@ if [ "$package" == "all" ] || [ "$package" == "busybox" ]; then
 fi
 if [ "$package" == "all" ] || [ "$package" == "nftables" ]; then
   ver="1.1.6";
-  if [ ! -f "download/nftables-$ver.tar.xz" ]; then
-    wget -O download/nftables-$ver.tar.xz https://netfilter.org/projects/nftables/files/nftables-1.1.6.tar.xz
-  fi
-  if [ ! -d "out/nftables/nftables-$ver" ]; then
-    mkdir out/nftables || true
-    cd out/nftables
-    tar -xvf ../../download/nftables-$ver.tar.xz
-    cd ../..
-  fi
+  download_unpack https://netfilter.org/projects/nftables/files/nftables-1.1.6.tar.xz nftables nftables-$ver
   mkdir app/nftables || true
   mkdir app/nftables/$ver || true
   cd out/nftables/nftables-$ver
@@ -71,15 +65,7 @@ if [ "$package" == "all" ] || [ "$package" == "nftables" ]; then
 fi
 if [ "$package" == "all" ] || [ "$package" == "bwrap" ]; then
   ver="0.11.2";
-  if [ ! -f "download/bubblewrap-$ver.tar.xz" ]; then
-    wget -O download/bubblewrap-$ver.tar.xz https://github.com/containers/bubblewrap/releases/download/v$ver/bubblewrap-$ver.tar.xz
-  fi
-  if [ ! -d "out/bwrap/bubblewrap-$ver" ]; then
-    mkdir out/bwrap || true
-    cd out/bwrap
-    tar -xvf ../../download/bubblewrap-$ver.tar.xz
-    cd ../..
-  fi
+  download_unpack https://github.com/containers/bubblewrap/releases/download/v$ver/bubblewrap-$ver.tar.xz bwrap bubblewrap-$ver
   mkdir app/bwrap || true
   mkdir app/bwrap/$ver || true
   cp in/bwrap/*.c out/bwrap/bubblewrap-$ver
