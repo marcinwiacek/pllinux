b8e9cba69150f97d9dd42ffde06810632198490f
Marcin Wiącek <marcin@mwiacek.com>
2026-07-08 22:31:36 +0200

    Updates

--- doit/in/etc/users/root.sh
+++ doit/in/etc/users/root.sh
@@ -1,6 +1,6 @@
 #!/app/busybox/current/bin/sh
 cd /
-export PATH=/app/kbd/current:/app/nftables/current:/app/dinit/current/:/app/util-linux/current:/app/mc/current:/app/busybox/current/bin:/app/busybox/current/sbin:/app/gnupg/current:/app/pllinux/current/helper:/app/openssl/current
+export PATH=/app/rsync/current:/app/wget2/current:/app/kbd/current:/app/nftables/current:/app/dinit/current/:/app/util-linux/current:/app/mc/current:/app/busybox/current/bin:/app/busybox/current/sbin:/app/gnupg/current:/app/pllinux/current/helper:/app/openssl/current
 export SHELL=/app/busybox/current/bin/sh
 export HOME=/other/app/sh
 mkdir $HOME 2> /dev/null
