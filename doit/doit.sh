beada4c5efa949fa0d1968c4de88ad124ba5c539
Marcin Wiącek <marcin@mwiacek.com>
2026-07-13 22:46:10 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 11 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="gcc"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.)
+package="pllinux"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.)
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 
@@ -846,8 +846,9 @@ if [ "$package" == "glib" ]; then
     fi
     meson compile -C _build
     meson install -C _build
-    chmod a-x $output/app/glib/$prefix$ver/lib/*
     cd ../../..
+    chmod a+x $output/app/glib/$prefix$ver/lib/x86_64-linux-gnu/
+    chmod a-x $output/app/glib/$prefix$ver/lib/x86_64-linux-gnu/*so*
     rsync -a $output/app/glib/$prefix$ver/lib/x86_64-linux-gnu/* $output/app/glib/$prefix$ver/lib
     rm -r $output/app/glib/$prefix$ver/lib/x86_64-linux-gnu/
     set_current_app glib $prefix$ver
