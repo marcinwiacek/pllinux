e257a4939ee4c6cd39d27f0bcde1c4a1eaf96f53
Marcin Wiącek <marcin@mwiacek.com>
2026-07-16 21:47:22 +0200

    Updates

--- doit/in/etc/users/root.sh
+++ doit/in/etc/users/root.sh
@@ -4,5 +4,8 @@ export PATH=/app/rsync/current:/app/wget2/current:/app/kbd/current:/app/nftables
 export SHELL=/app/busybox/current/bin/sh
 export HOME=/other/app/sh
 mkdir $HOME 2> /dev/null
-export PS1="\e[31m[\t] \u:\w # \e[0m"  # red [time] user : folder #
+# red [time] user:folder #
+# OR
+# red [time] user:folder amber git branch name #
+export PS1="\e[31m[\t] root:\w\$(if [ -f "/app/git/current/git" ] && /app/git/current/git rev-parse --git-dir > /dev/null 2>&1; then echo -n \" \e[33m\";/app/git/current/git branch --show-current; fi) # \e[m"
 sh
