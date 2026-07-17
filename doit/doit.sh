6ed783536c886d37e4dedf0316075c27eba053fd
Marcin Wiącek <marcin@mwiacek.com>
2026-07-17 23:45:17 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 11 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="automake"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.)
+package="gcc"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.)
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=0; # 1 - some compilations will be done in RAM disk; 0 - save all to disk
@@ -833,6 +833,7 @@ if [ "$package" == "fs" ] || [ "$package" == "gcc" ]; then
     set_current_app gcc $prefix$ver
     strip_app gcc
     rsync -a in/gcc/ $output/app/gcc/$prefix$ver
+    remove_duplicates $output/app/gcc/$prefix$ver/bin
   fi
 fi
 if [ "$package" == "fs" ] || [ "$package" == "slang" ]; then
