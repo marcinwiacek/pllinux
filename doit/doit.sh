ab5e63ffbcb4db79a84aa3dd297a3e95cd903efd
Marcin Wiącek <marcin@mwiacek.com>
2026-07-27 22:28:33 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="glib"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+package="gcc"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -741,6 +741,7 @@ if [ "$package" == "fs" ] || [ "$package" == "gcc" ]; then
     rm -r $output/app/gcc/$prefix$ver/lib64
     rsync -a $curdir/in/gcc/ $output/app/gcc/$prefix$ver
     remove_duplicates_cd $output/app/gcc/$prefix$ver/bin
+    remove_duplicates_cd $output/app/gcc/$prefix$ver/share/man/man1
     set_current_app_clean_strip_cd gcc $prefix$ver 1
   fi
 fi
