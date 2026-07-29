32ccb0af0461ac59b38c8f73f3aa9438ea10fc03
Marcin Wiącek <marcin@mwiacek.com>
2026-07-29 22:52:10 +0200

    Updates

--- doit/in/pllinux/scripts/boot.sh
+++ doit/in/pllinux/scripts/boot.sh
@@ -23,31 +23,15 @@ fi
 /app/util-linux/current/bin/mount mount -t tmpfs -o rw,noatime,nosuid,noexec,mode=1777 /tmp
 /app/busybox/current/bin/chmod a+rw /dev/null
 
+# access to dinit for non-root users
+/app/busybox/current/bin/busybox chmod a+rw /run/dinitctl
+
 # starts and configures automatic mounting devices (USB pendrives, memory cards, etc.)
 # (enable mdev on request and process already connected devices)
 /app/busybox/current/bin/echo /app/busybox/current/sbin/mdev > /proc/sys/kernel/hotplug
 /app/busybox/current/sbin/mdev -s
 
-#export LANG=pl_PL.utf8
-
-# console font
-#/app/kbd/current/bin/setfont -C /dev/tty1 sun12x22.psfu.gz 2> /dev/null
-#/app/kbd/current/bin/setfont -C /dev/tty2 sun12x22.psfu.gz 2> /dev/null
-#/app/kbd/current/bin/setfont -C /dev/tty3 sun12x22.psfu.gz 2> /dev/null
-#/app/kbd/current/bin/setfont -C /dev/tty4 sun12x22.psfu.gz 2> /dev/null
-#/app/kbd/current/bin/setfont -C /dev/tty1 lat9u-16.psfu.gz 2> /dev/null
-#/app/kbd/current/bin/setfont -C /dev/tty1 solar24x32.psfu.gz 2> /dev/null
-
-# keyboard
-#/app/kbd/current/bin/loadkeys /app/kbd/current/share/keymaps/i386/qwerty/pl4.map.gz
-
-# localtime
-ln -s /app/tzdb/current/usr/share/zoneinfo/Europe/Warsaw /etc/localtime 2> /dev/null
-
 # firewall rules
 /app/nftables/current/sbin/nft -f /etc/network/nftables/inet-filter.nft
 
-# access to dinit for non-root users
-/app/busybox/current/bin/busybox chmod a+rw /run/dinitctl
-
-exec /etc/boot
+pllinux /etc/pllinux.cfg
