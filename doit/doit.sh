0f093819b948d5c574a6d54df7d4ced6d3d79e09
Marcin Wiącek <marcin@mwiacek.com>
2026-08-11 23:15:36 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="cryptsetup"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+package="libselinux"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -999,6 +999,17 @@ if [ "$package" == "fs" ] || [ "$package" == "cryptsetup" ]; then
     create_app cryptsetup $prefix$ver
     make install
     set_current_app_clean_strip_cd cryptsetup $prefix$ver 1
+  fi
+fi
+if [ "$package" == "fs" ] || [ "$package" == "libselinux" ]; then
+  ver="3.11";
+  if should_make libselinux $ver; then
+    download_unpack_source https://github.com/SELinuxProject/selinux/releases/download/$ver/libselinux-$ver.tar.gz libselinux libselinux-$ver 1
+    create_app libselinux $prefix$ver
+    make -j$cpu_num DESTDIR=$output/app/libselinux/$prefix$ver install
+    cd $output/app/libselinux/$prefix$ver/lib
+    ln -s libselinux.so.1 libselinux.so
+    set_current_app_clean_strip_cd libselinux $prefix$ver 1
   fi
 fi
 #if [ "$package" == "gpm" ]; then
