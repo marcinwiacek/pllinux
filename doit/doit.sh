dbe5cfc0298cd9df580ab243ba1b5109e4a8edce
Marcin Wiącek <marcin@mwiacek.com>
2026-08-13 22:40:43 +0200

    updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="dialog"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+package="rsync"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -645,7 +645,7 @@ if [ "$package" == "fs" ] || [ "$package" == "wget2" ]; then
   fi
 fi
 if [ "$package" == "fs" ] || [ "$package" == "rsync" ]; then
-  ver="3.4.4";
+  ver="3.5.0";
   if should_make rsync $ver; then
     download_unpack_source https://download.samba.org/pub/rsync/src/rsync-$ver.tar.gz rsync rsync-$ver 1
     ./configure --disable-xxhash --disable-lz4
