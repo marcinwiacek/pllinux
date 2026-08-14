a2f29465a4b11badabb80e7751e9469722611edf
Marcin Wiącek <marcin@mwiacek.com>
2026-08-14 22:10:42 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="popt"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+package="nano"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -1035,6 +1035,17 @@ if [ "$package" == "fs" ] || [ "$package" == "gptfdisk" ]; then
     cp fixparts $output/app/gptfdisk/$prefix$ver/bin
     cp *.8 $output/app/gptfdisk/$prefix$ver/man
     set_current_app_clean_strip_cd gptfdisk $prefix$ver 1
+  fi
+fi
+if [ "$package" == "fs" ] || [ "$package" == "nano" ]; then
+  ver="9.2";
+  if should_make nano $ver; then
+    download_unpack_source https://www.nano-editor.org/dist/v9/nano-$ver.tar.xz nano nano-$ver 1
+    ./configure --prefix=$output/app/nano/$prefix$ver
+    make -j$cpu_num
+    create_app nano $prefix$ver
+    make install
+    set_current_app_clean_strip_cd nano $prefix$ver 1
   fi
 fi
 #if [ "$package" == "gpm" ]; then
