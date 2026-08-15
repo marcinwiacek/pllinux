af4b6177f6be22a135b414bd9eb899db701a10e9
Marcin Wiącek <marcin@mwiacek.com>
2026-08-16 00:50:01 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="dinit"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+package="util-linux"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -398,6 +398,8 @@ if [ "$package" == "fs" ] || [ "$package" == "util-linux" ]; then
     ./configure --prefix=$output/app/util-linux/$prefix$ver --without-systemd --disable-lsfd --disable-enosys
     make all -j$cpu_num
     create_app util-linux $prefix$ver
+    #fakeroot
+    #fixme: tries to remove files from /usr/share... doesn't respect --prefix
     make install || true
     cp $curdir/in/util-linux/* $output/app/util-linux/$prefix$ver
     set_current_app_clean_strip_cd util-linux $prefix$ver 1
