00594150a8b7b2e923b72182173029e1c84c3fec
Marcin Wiącek <marcin@mwiacek.com>
2026-09-07 22:56:52 +0200

    updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
-output="/pllinux";  # directory with EXT4 partition, which will be / for new system
-package="busybox"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
+output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
+package="efibootmgr"; # "fs" to build all or concrete name for concrete package (busybox, nftables, etc.) or iso to build iso file
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -1057,7 +1057,7 @@ if [ "$package" == "fs2" ] || [ "$package" == "read-edid" ]; then
 #needs cmake version in cmakelists.txt
     cmake .
     make 
-make install
+    make install
 #    create_app read-edid $prefix$ver
 #    mkdir $output/app/read-edid/$prefix$ver/bin
 #    for binentry in example lspci pcilmr setpci update-pciids update-pciids.sh; do
@@ -1147,6 +1147,19 @@ if [ "$package" == "fs" ] || [ "$package" == "perl" ]; then
     ./perl -Ilib -I. installperl --destdir=$output/app/perl/$prefix$ver
     remove_duplicates_cd $output/app/perl/$prefix$ver/usr/local/bin
     set_current_app_clean_strip_cd perl $prefix$ver 1
+  fi
+fi
+if [ "$package" == "fs2" ] || [ "$package" == "efibootmgr" ]; then
+  #work in progress
+  ver="18";
+  if should_make efibootmgr $ver; then
+    download_unpack_source https://github.com/rhboot/efibootmgr/releases/download/$ver/efibootmgr-$ver.tar.bz2 efibootmgr efibootmgr-$ver 1
+#    ./Configure -d #-Dprefix=$output/app/perl/$prefix$ver
+#    make -j$cpu_num
+#    create_app perl $prefix$ver
+#    ./perl -Ilib -I. installperl --destdir=$output/app/perl/$prefix$ver
+#    remove_duplicates_cd $output/app/perl/$prefix$ver/usr/local/bin
+#    set_current_app_clean_strip_cd perl $prefix$ver 1
   fi
 fi
 # rhboot/shim
