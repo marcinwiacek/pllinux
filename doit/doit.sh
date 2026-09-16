cbecad55d7e22bea01844f613bcb25cb91909044
Marcin Wiącek <marcin@mwiacek.com>
2026-09-17 00:46:57 +0200

    updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="brotli"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
+package="chezscheme"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -161,6 +161,7 @@ remove_duplicates_cd() {
       for fname2 in $(find . -maxdepth 1 -type f); do
         CHECKSUM2=($(md5sum $fname2))
         if [ "$fname" != "$fname2" ] && [ "$CHECKSUM" = "$CHECKSUM2" ]; then
+            chmod a+w $fname2
             rm $fname2
             ln -s $fname $fname2
             DOITAGAIN="1"
@@ -1284,6 +1285,7 @@ if [ "$package" == "fs" ] || [ "$package" == "chezscheme" ]; then
     make -j$cpu_num
     create_app chezscheme $prefix$ver
     make install
+    remove_duplicates_cd $output/app/chezscheme/$prefix$ver/lib/csv$ver/ta6le
     set_current_app_clean_strip_cd chezscheme $prefix$ver 1
   fi
 fi
