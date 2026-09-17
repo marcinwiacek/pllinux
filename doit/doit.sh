6333351e3f6fd7669aeba3c9d318bb4735b1b82e
Marcin Wiącek <marcin@mwiacek.com>
2026-09-17 22:39:51 +0200

    updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="chezscheme"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
+package="kernel"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -230,11 +230,13 @@ if [ "$package" == "fs" ] || [ "$package" == "fsmin" ] || [ "$package" == "kerne
     # no compilation in ram
     download_unpack_source https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-$ver.tar.xz kernel linux-$ver 0
     cp $curdir/in/kernel/.config $curdir/out/kernel/linux-$ver
-    make -j$cpu_num
+#    make -j$cpu_num
     cp .config $curdir/in/kernel # .config will be updated with new header and maybe options
     create_app kernel $prefix$ver
     cp $curdir/in/kernel/.config $output/app/kernel/$prefix$ver
     cp $curdir/out/kernel/linux-$ver/arch/x86/boot/bzImage $output/app/kernel/$prefix$ver
+    make modules_install INSTALL_MOD_PATH=/tmp/x
+    make headers_install INSTALL_HDR_PATH=$output/app/kernel/$prefix$ver
     set_current_app_clean_strip_cd kernel $prefix$ver 1
   fi
 fi
