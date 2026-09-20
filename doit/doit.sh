891d5ec62effe1aa750d5a9b17e4269c1101acd7
Marcin Wiącek <marcin@mwiacek.com>
2026-09-21 00:39:40 +0200

    Updates

--- doit/doit.sh
+++ doit/doit.sh
@@ -1,7 +1,7 @@
 # Part of PLLINUX. Version from 23 July 2026. Creating binaries (from the source) and installing them in the PLLINUX partition. Tested on Debian "Trixie".
 
 output="/mnt/x";  # directory with EXT4 partition, which will be / for new system
-package="libfastjson"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
+package="usbutils"; # "fs" to build all, "fsmin" to build minimalistic working system, "iso" to build iso file or concrete name for package (busybox, nftables, etc.)
 cpu_num=6; # how many CPU cores are used during compilation
 dont_process_the_same_ver=0; # 1 - on; 0 - off; don't compile and install app, when the same version (even from other day) available
 use_tmpfs=1; # 1 - some compilations will be done in RAM disk (currently excluded: kernel and gcc part); 0 - save all to disk
@@ -1320,5 +1320,20 @@ if [ "$package" == "fs" ] || [ "$package" == "fsmin" ] || [ "$package" == "brotl
     create_app brotli $prefix$ver
     cmake --build . --config Release --target install
     set_current_app_clean_strip_cd brotli $prefix$ver 1
+  fi
+fi
+if [ "$package" == "fs2" ] || [ "$package" == "usbutils" ]; then
+  #work in progress
+  ver="019";
+  if should_make usbutils $ver; then
+    download_unpack_source https://github.com/gregkh/usbutils/archive/refs/tags/v$ver.tar.gz usbutils usbutils-$ver 1
+    install_host_deps "libusb-dev libudev-dev"
+
+    meson setup -Dprefix=$output/app/usbutils/$prefix$ver _build
+    meson compile -C _build
+    create_app usbutils $prefix$ver
+    meson install -C _build
+
+#    set_current_app_clean_strip_cd usbutils $prefix$ver 1
   fi
 fi
